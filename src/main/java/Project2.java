e2f4bfdea453c2df943d6c16433419808366d6bd
Elif Salman <elifsalman94@hotmail.com>
2018-11-12 13:16:14 +0300

    add client tcp

--- src/main/java/Project2.java
+++ src/main/java/Project2.java
@@ -4,6 +4,8 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -17,22 +19,24 @@ public class Project2 {
     public static final String SERVER_ADDRESS = "localhost";
 
     public static void main(String[] arg){
-        System.out.println("Write 0 for Server , 1 for Client");
-        int mode = scanner.nextInt();
+        System.out.println("Write s for Server , c for Client");
+        String mode = scanner.nextLine();
 
-        System.out.println("Write 0 for TCP , 1 for SSL");
-        int conType = scanner.nextInt();
+        System.out.println("Write t for TCP , s for SSL");
+        String conType = scanner.nextLine();
 
 // TCP -----------------------------------------------------------------------------------
-        if(conType == 0){
+        if(conType.equals("t")){
             // Server
-            if(mode == 0){
+            if(mode.equals("s")){
                 try {
                     ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+                    System.out.println("Waiting connections...");
                     HashMap<String, String> map = new HashMap<>();
                     while(true){
                         Socket socket = serverSocket.accept();
                         new Thread(() -> {
+                            System.out.println("New connection established.");
                             try {
                                 BufferedReader inStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                                 PrintWriter outStream = new PrintWriter(socket.getOutputStream());
@@ -42,7 +46,7 @@ public class Project2 {
                                     message = inStream.readLine();
                                     System.out.println("Client: "+message);
                                     if(message.contains("submit") && message.contains(",")){
-                                        String parts[] = message.split(",\\s*");
+                                        String parts[] = message.split("\\s*(,|\\s)\\s*");
                                         map.put(parts[1], parts[2]);
                                         System.out.println("entry saved, map size="+map.size());
                                         outStream.println("OK");
@@ -63,8 +67,47 @@ public class Project2 {
             }
 
             // Client
-            else if(mode == 1){
+            else if(mode.equals("c")){
+                try {
+                    Socket socket =new Socket(SERVER_ADDRESS, SERVER_PORT);
+                    socket.setSoTimeout(2000);
+                    BufferedReader inStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    PrintWriter outStream = new PrintWriter(socket.getOutputStream());
 
+                    String message, response;
+                    while (true){
+                        message = scanner.nextLine();
+                        String parts[] = message.split("\\s*(,|\\s)\\s*");
+                        if(!parts[0].equals("submit") && !parts[0].equals("get")){
+                            System.out.print("Error: wrong command format: " + parts.length);
+                            System.out.println(Arrays.asList(parts).toString());
+                            continue;
+                        }
+
+                        while(true) {
+                            outStream.println(message);
+                            outStream.flush();
+                            try {
+                                response = inStream.readLine();
+                                if (parts[0].equals("submit") && response.equals("OK")) {
+                                    System.out.format("Successfully submitted <%s, %s> to server at IP address of %s"
+                                            , parts[1], parts[2], SERVER_ADDRESS);
+                                    break;
+                                }
+                                else if(parts[0].equals("get")){
+                                    System.out.println("Server: "+response);
+                                    break;
+                                }
+                            } catch (SocketTimeoutException e) {
+                                System.out.println("Timeout: Resending");
+                            }
+                        }
+                    }
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
 
         }
 
